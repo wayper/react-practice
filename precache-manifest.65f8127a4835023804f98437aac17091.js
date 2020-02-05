c6e3bab603442612d69c49c27283809c848a1834
Dmitry Ivanov <wayper.plenitude@gmail.com>
2020-02-05 14:57:59 +0200

    Updates

---
+++ precache-manifest.65f8127a4835023804f98437aac17091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b185eaa9e6af7b1ab8aa65687cc7c993",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "5a7a36d04f1dcec3a34d",
+    "url": "/react-practice/static/css/main.69868bea.chunk.css"
+  },
+  {
+    "revision": "c5a23e21fd0dc64376b9",
+    "url": "/react-practice/static/js/2.9e3e8dce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-practice/static/js/2.9e3e8dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7a36d04f1dcec3a34d",
+    "url": "/react-practice/static/js/main.68fe9c37.chunk.js"
+  },
+  {
+    "revision": "646aa7a79051e7902e4b",
+    "url": "/react-practice/static/js/runtime-main.141b366e.js"
+  }
+]);
